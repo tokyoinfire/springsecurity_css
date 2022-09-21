fe9bee943170d54ede8406c940577e7d517a15b7
sveklenko15@gmail.com <0995658527Satan>
2022-09-21 16:06:26 +0300

    Added modal windows. Task done.

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -6,54 +6,48 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.entity.User;
 import ru.kata.spring.boot_security.demo.repository.RoleRepository;
-import ru.kata.spring.boot_security.demo.service.UserService;
+import ru.kata.spring.boot_security.demo.service.UserServiceImp;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
-    private final UserService userService;
+    private final UserServiceImp userService;
     private final RoleRepository roleRepository;
 
     @Autowired
-    public AdminController(UserService userService, RoleRepository roleRepository) {
+    public AdminController(UserServiceImp userService, RoleRepository roleRepository) {
         this.userService = userService;
         this.roleRepository = roleRepository;
+
     }
 
-    @GetMapping
-    public String index(Model model) {
-        model.addAttribute("users", userService.listUsers());
-        return "/admin/index";
-    }
-
-    @GetMapping("/new")
-    public String newUser(@ModelAttribute("user") User user, Model model) {
-        model.addAttribute("listRoles", roleRepository.findAll());
-        return "/admin/new";
-    }
-
-    @PostMapping
-    public String create(@ModelAttribute("user") User user) {
+    @PostMapping("/createNewUser")
+    public String createNewUser(@ModelAttribute("user") User user) {
         userService.add(user);
         return "redirect:/admin";
     }
 
-    @GetMapping("/{id}")
-    public String edit(Model model, @PathVariable("id") long id) {
-        model.addAttribute("user", userService.getUserByID(id));
-        model.addAttribute("listRoles", roleRepository.findAll());
-        return "/admin/edit";
+
+    @GetMapping()
+    public String showAllUsers(Principal principal, Model model) {
+        model.addAttribute("users", userService.listUsers());
+        model.addAttribute("user", userService.loadUserByUsername(principal.getName()));
+        model.addAttribute("list", roleRepository.findAll());
+        return "admin/index";
     }
 
-    @PutMapping("/{id}")
-    public String update(@ModelAttribute("user") User user) {
+
+    @PatchMapping("/{id}/updateUser")
+    public String showUpdatedUser(@PathVariable("id") int id, User user) {
         userService.update(user);
-        return "redirect:/admin/";
+        return "redirect:/admin";
     }
 
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") long id) {
+    public String delete(@PathVariable("id") int id) {
         userService.delete(id);
         return "redirect:/admin";
     }
